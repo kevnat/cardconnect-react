a2496cf381623823306193cc75f179e611c6d2f6
kevnat <kevmaxnathan@gmail.com>
2019-10-06 20:24:50 -0400

    tokenizer cleanupTODO: onBlur

--- client/src/components/tokenizer/tokenizer.js
+++ client/src/components/tokenizer/tokenizer.js
@@ -4,24 +4,34 @@ import API from '../../utils/API'
 export default class Tokenizer extends Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {
+          value: '',
+          token: ''
+        };
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
+    componentDidMount () {
+        document.addEventListener('keydown', this.handleHitEnter, true)
+      }
+      
+    
     handleChange(event) {
       this.setState({value: event.target.value});
     }
   
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
       event.preventDefault();
       let cardNum = this.state.value;
       API.tokenize(cardNum)
         .then(result => {
-            console.log(result);
+            this.setState({token: result.data.token})
         }); 
+    }
+    componentDidUpdate() {
+        document.removeEventListener('keydown', this.handleHitEnter, true)
     }
 
     render() {
@@ -29,14 +39,42 @@ export default class Tokenizer extends Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Card Number:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input 
+            type="text" 
+            value={this.state.value} 
+            onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
           <label>
               Token:
-            <pre>4444333322221111</pre>
+            <pre>{this.state.token}</pre>
           </label>
         </form>
       );
     }
   }
+
+
+/*
+ export default function CreditCardForm() {
+    const [values, setValues] = React.useState({
+      name: "",
+      number: "",
+      expiration: "",
+      cvc: ""
+    });
+    const handleChange = React.useCallback(
+      event => {
+        const { name, value } = event.target;
+        setValues(v => ({ ...v, [name]: value }));
+      },
+      [setValues]
+    );
+  
+    const [focused, setFocus] = React.useState<FOCUS_TYPE | undefined>(undefined);
+    const handleFocus = React.useCallback(
+      event => setFocus(event.target.name as FOCUS_TYPE),
+      [setFocus]
+    );
+    const handleBlur = React.useCallback(() => setFocus(undefined), [setFocus]);
+    */
